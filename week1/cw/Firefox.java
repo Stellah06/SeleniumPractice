faf8636f5aa457310e49d5fd3bdac2e11df36faa
Stellah06 <110719074+Stellah06@users.noreply.github.com>
2023-10-27 18:44:06 +0530

    Add files via upload
    Week 1 assignment

---
+++ week1/cw/Firefox.java
@@ -0,0 +1,18 @@
+package week1.cw;
+
+public class Firefox {
+
+    public static void main(String[] args) {
+        float browserVersion=100.2f;
+        String browserName="Firefox";
+        boolean isVisible=true;
+        int releaseYear=1998;
+        char browserLogo='f';
+
+        System.out.println("Browser version is " + browserVersion);
+        System.out.println("Browser name is " +browserName );
+        System.out.println("Browser is visible " + isVisible);
+        System.out.println("Browser release Year "+ releaseYear);
+        System.out.println("Browser Logo is "+ browserLogo);
+    }
+}
---
+++ week1/cw/HelloTestLeaf.java
@@ -0,0 +1,14 @@
+package week1.cw;
+
+public class HelloTestLeaf {
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to Test Leaf");
+        System.out.println("Commented");
+        System.out.println("Commented 2");
+
+
+    }
+
+}
